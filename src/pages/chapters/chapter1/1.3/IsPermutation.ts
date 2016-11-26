685c047a2b1ca71d660604a21a65b58f04cf1e31
Matt Daisley <mattdaisley@gmail.com>
2016-11-25 21:53:00 -0700

    problem 1.3 solution

--- src/pages/chapters/chapter1/1.3/IsPermutation.ts
+++ src/pages/chapters/chapter1/1.3/IsPermutation.ts
@@ -2,8 +2,12 @@ export class IsPermutation {
     
     validate( input1, input2 ): boolean {
 
-        // let arrInput: Array<string> = input.split('').sort();
-        
+        if ( input1.length !== input2.length ) return false;
+
+        let str1 = input1.split('').sort().join();
+        let str2 = input2.split('').sort().join();
+
+        if ( str1 !== str2 ) return false;
 
         return true;
     }
