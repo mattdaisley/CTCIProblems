40439f2e9a526061915f490f5f2d7516fabc55d8
Matt Daisley <mattdaisley@gmail.com>
2016-11-27 22:56:35 -0700

    copy bash script commit

---
+++ bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# My first script
+
+STARTCOMMIT="$(git log --pretty=format:"%H" -1)"
+
+git pull origin master
+
+ENDCOMMIT="$(git log --pretty=format:"%H" -1)"
+# echo "${COMMITID}"
+
+# mkdir -p mattBuild
+
+git diff --name-only $STARTCOMMIT..$ENDCOMMIT) | {
+  while IFS= read -r line
+  do
+    echo "copying $line"
+    # rsync -R "$line" mattBuild
+  done
+
+}
