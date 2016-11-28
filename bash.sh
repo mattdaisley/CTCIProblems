93396bc651a23ef27d164163fb5ef7a31ef63bf3
Matt Daisley <mattdaisley@gmail.com>
2016-11-27 23:01:21 -0700

    add if check and echoes

--- bash.sh
+++ bash.sh
@@ -2,19 +2,23 @@
 # My first script
 
 STARTCOMMIT="$(git log --pretty=format:"%H" -1)"
+echo "$STARTCOMMIT"
 
 git pull origin master
 
 ENDCOMMIT="$(git log --pretty=format:"%H" -1)"
+echo "$ENDCOMMIT"
 # echo "${COMMITID}"
 
 # mkdir -p mattBuild
 
-git diff --name-only $STARTCOMMIT..$ENDCOMMIT | {
-  while IFS= read -r line
-  do
-    echo "copying $line"
-    # rsync -R "$line" mattBuild
-  done
+if [ "$STARTCOMMIT" != '$ENDCOMMIT' ]; then
+    git diff --name-only $STARTCOMMIT..$ENDCOMMIT | {
+    while IFS= read -r line
+    do
+        echo "copying $line"
+        # rsync -R "$line" mattBuild
+    done
 
-}
+    }
+fi
