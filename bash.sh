8136b523e253c1859c309d86c2af80295e54430a
Matt Daisley <mattdaisley@gmail.com>
2016-11-27 23:02:15 -0700

    add rsync command and mkdir

--- bash.sh
+++ bash.sh
@@ -10,14 +10,14 @@ ENDCOMMIT="$(git log --pretty=format:"%H" -1)"
 echo "$ENDCOMMIT"
 # echo "${COMMITID}"
 
-# mkdir -p mattBuild
+mkdir -p mattBuild
 
 if [ "$STARTCOMMIT" != '$ENDCOMMIT' ]; then
     git diff --name-only $STARTCOMMIT..$ENDCOMMIT | {
     while IFS= read -r line
     do
         echo "copying $line"
-        # rsync -R "$line" mattBuild
+        rsync -R "$line" mattBuild
     done
 
     }
