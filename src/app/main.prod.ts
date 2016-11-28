b6cf36e228e89af031cbec973c954aab729a7399
Matt Daisley <mattdaisley@gmail.com>
2016-11-27 23:05:05 -0700

    in multiple commits

--- src/app/main.prod.ts
+++ src/app/main.prod.ts
@@ -4,4 +4,5 @@ import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './app.module.ngfactory';
 
 enableProdMode();
+
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
